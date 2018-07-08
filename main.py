23c42b23ff80825eb31814b760580af254a613b0
jean3087m <unnchat@gmail.com>
2018-07-08 15:01:45 -0300

    new main

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/bash/python3
+
+import pygame 
+from pygame.locals import *
+
+import PIL
+import json
+import sys 
+import os 
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
